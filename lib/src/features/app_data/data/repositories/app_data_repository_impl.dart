fec84bba9565db579c1bd583abdb4b1a0af13c2c
Darinel Escobar <105504377+DarinelEscobar@users.noreply.github.com>
2026-01-13 14:55:14 -0600

    Fix export to locate SQLite database

--- lib/src/features/app_data/data/repositories/app_data_repository_impl.dart
+++ lib/src/features/app_data/data/repositories/app_data_repository_impl.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:sqflite/sqflite.dart';
 import '../../../../data/schema/schemas.dart';
 import '../../domain/repositories/app_data_repository.dart';
 
